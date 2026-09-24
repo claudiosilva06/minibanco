3e879020dcff65dd5826a6fc7176082285429164
claudiosilva06 <320584118+claudiosilva06@users.noreply.github.com>
2026-09-24 20:25:27 -0300

    chore: cria projeto Spring Boot do mini banco

---
+++ src/main/java/br/com/claudio/minibanco/MinibancoApplication.java
@@ -0,0 +1,13 @@
+package br.com.claudio.minibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/claudio/minibanco/MinibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claudio.minibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
